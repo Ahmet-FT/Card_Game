1c5d7c5f56e9db525779a5eb09ef70e78ab9de61
Ahmet-FT <ahmetf.tekin@gmail.com>
2024-11-16 16:56:22 +0300

    main

---
+++ Oyun/Program.cs
@@ -0,0 +1,62 @@
+﻿using Main;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Baslangic seviyesi girin: ");
+        int baslangic = Convert.ToInt32(Console.ReadLine());
+
+        Ucak ucak = new Ucak(baslangic);
+
+        Ucak ucak2 = new Ucak(baslangic);
+
+        ucak.KartPuaniGoster();
+
+        ucak.DurumGuncelle(ucak2.Vurus);
+        ucak.DurumGuncelle(ucak2.Vurus);
+
+        if ("merhaba" == "merhaba")
+        {
+            Console.WriteLine("ise yariyor.");
+        }
+        else
+        {
+            Console.WriteLine("ise yaramiyor");
+        }
+
+
+    }
+}
+
+class Vehicle
+{
+    public int speed = 0;
+
+    public String name;
+
+    public void go()
+    {
+        Console.WriteLine($"{name} is moving at {speed} km/h.");
+
+    }
+}
+
+class Car : Vehicle
+{
+    public Car()
+    {
+        name = "car";
+    }
+}
+
+class Bicycle : Vehicle
+{
+    public int wheels = 2;
+}
+class Boat : Vehicle
+{
+    public int wheels = 0;
+}
---
+++ Oyun/Savas_Araclari.cs
@@ -0,0 +1,184 @@
+﻿using System;
+
+namespace Main
+{
+    // Abstract Savaş Araçları Sınıfı
+    abstract class Savas_Araclari
+    {
+        public int SeviyePuani { get; set; }
+        public abstract int Dayaniklilik { get; set; }
+        public abstract int Vurus { get; set; }
+        public abstract string Sinif { get; }
+
+        // Kart puanı göstermek için metod
+        public void KartPuaniGoster()
+        {
+            Console.WriteLine($"Dayaniklilik: {Dayaniklilik}, Seviye Puani: {SeviyePuani}, Sınıf: {Sinif}");
+        }
+
+        // Abstract metod: Durum güncelleme
+        public abstract void DurumGuncelle(int vurduguHasar);
+    }
+
+    // Abstract Hava Araçları Sınıfı
+    abstract class Hava_Araclari : Savas_Araclari
+    {
+        public abstract string AltSinif { get; }
+        public abstract int KaraVurusAvantaji { get; }
+
+        // Constructor
+        protected Hava_Araclari(int seviyePuani)
+        {
+            SeviyePuani = seviyePuani;
+        }
+    }
+
+    // Uçak Sınıfı
+    class Ucak : Hava_Araclari
+    {
+        public override int Dayaniklilik { get; set; } = 20;
+        public override int Vurus { get; set; } = 10;
+        public override string Sinif => "Hava";
+        public override string AltSinif => "Uçak";
+        public override int KaraVurusAvantaji => 10;
+
+        // Constructor
+        public Ucak(int seviyePuani) : base(seviyePuani) { }
+
+        // Durum güncelleme
+        public override void DurumGuncelle(int vurduguHasar)
+        {
+            Dayaniklilik -= vurduguHasar;
+            SeviyePuani += vurduguHasar;
+            Console.WriteLine($"Uçak Durumu Güncellendi: Dayaniklilik = {Dayaniklilik}, SeviyePuani = {SeviyePuani}");
+        }
+    }
+
+    // SİHA Sınıfı
+    class Siha : Hava_Araclari
+    {
+        public override int Dayaniklilik { get; set; } = 15;
+        public override int Vurus { get; set; } = 10;
+        public override string Sinif => "Hava";
+        public override string AltSinif => "Siha";
+        public override int KaraVurusAvantaji => 10;
+
+        // Constructor
+        public Siha(int seviyePuani) : base(seviyePuani) { }
+
+        // Durum güncelleme
+        public override void DurumGuncelle(int vurduguHasar)
+        {
+            Dayaniklilik -= vurduguHasar;
+            SeviyePuani += vurduguHasar;
+            Console.WriteLine($"{AltSinif} Durumu Güncellendi: Dayaniklilik = {Dayaniklilik}, SeviyePuani = {SeviyePuani}");
+        }
+    }
+
+    // Abstract Kara Araçları Sınıfı
+    abstract class Kara_Araclari : Savas_Araclari
+    {
+        public abstract string AltSinif { get; }
+        public abstract int DenizVurusAvantaji { get; }
+
+        protected Kara_Araclari(int seviyePuani)
+        {
+            SeviyePuani = seviyePuani;
+        }
+    }
+
+    // Obüs Sınıfı
+    class Obus : Kara_Araclari
+    {
+        public override int Dayaniklilik { get; set; } = 20;
+        public override int Vurus { get; set; } = 10;
+        public override string Sinif => "Kara";
+        public override string AltSinif => "Obüs";
+        public override int DenizVurusAvantaji => 5;
+
+        public Obus(int seviyePuani) : base(seviyePuani) { }
+
+        // Durum güncelleme
+        public override void DurumGuncelle(int vurduguHasar)
+        {
+            Dayaniklilik -= vurduguHasar;
+            SeviyePuani += vurduguHasar;
+            Console.WriteLine($"{AltSinif} Durumu Güncellendi: Dayaniklilik = {Dayaniklilik}, SeviyePuani = {SeviyePuani}");
+        }
+    }
+
+    // KFS Sınıfı
+    class KFS : Kara_Araclari
+    {
+        public override int Dayaniklilik { get; set; } = 10;
+        public override int Vurus { get; set; } = 10;
+        public override string Sinif => "Kara";
+        public override string AltSinif => "KFS";
+        public override int DenizVurusAvantaji => 10;
+
+        public KFS(int seviyePuani) : base(seviyePuani) { }
+
+
+        // Durum güncelleme
+        public override void DurumGuncelle(int vurduguHasar)
+        {
+            Dayaniklilik -= vurduguHasar;
+            SeviyePuani += vurduguHasar;
+            Console.WriteLine($"{AltSinif} Durumu Güncellendi: Dayaniklilik = {Dayaniklilik}, SeviyePuani = {SeviyePuani}");
+        }
+    }
+
+    // Abstract Deniz Araçları Sınıfı
+    abstract class Deniz_Araclari : Savas_Araclari
+    {
+        public abstract string AltSinif { get; }
+        public abstract int HavaVurusAvantaji { get; }
+
+        protected Deniz_Araclari(int seviyePuani)
+        {
+            SeviyePuani = seviyePuani;
+        }
+    }
+
+    // Firkateyn Sınıfı
+    class Fikrateyn : Deniz_Araclari
+    {
+        public override int Dayaniklilik { get; set; } = 25;
+        public override int Vurus { get; set; } = 10;
+        public override string Sinif => "Deniz";
+        public override string AltSinif => "Firkateyn";
+        public override int HavaVurusAvantaji => 5;
+
+        public Fikrateyn(int seviyePuani) : base(seviyePuani) { }
+
+
+        // Durum güncelleme
+        public override void DurumGuncelle(int vurduguHasar)
+        {
+            Dayaniklilik -= vurduguHasar;
+            SeviyePuani += vurduguHasar;
+            Console.WriteLine($"{AltSinif} Durumu Güncellendi: Dayaniklilik = {Dayaniklilik}, SeviyePuani = {SeviyePuani}");
+        }
+    }
+
+    // SİDA Sınıfı
+    class Sida : Deniz_Araclari
+    {
+        public override int Dayaniklilik { get; set; } = 15;
+        public override int Vurus { get; set; } = 10;
+        public override string Sinif => "Deniz";
+        public override string AltSinif => "Sida";
+        public override int HavaVurusAvantaji => 10;
+
+        public Sida(int seviyePuani) : base(seviyePuani) { }
+
+
+        // Durum güncelleme
+        public override void DurumGuncelle(int vurduguHasar)
+        {
+            Dayaniklilik -= vurduguHasar;
+            SeviyePuani += vurduguHasar;
+            Console.WriteLine($"{AltSinif} Durumu Güncellendi: Dayaniklilik = {Dayaniklilik}, SeviyePuani = {SeviyePuani}");
+        }
+    }
+}
